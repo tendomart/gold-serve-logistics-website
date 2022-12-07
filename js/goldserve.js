b8e4887ce53c99a9d00ad4064662aca743ab8d72
Tendo Martin <tmkmart@gmail.com>
2022-12-07 16:03:47 +0300

    Doing more changes

--- js/goldserve.js
+++ js/goldserve.js
@@ -1,160 +1,89 @@
-const $window = $(window);
-const $body = $('body');
-
-class Slideshow {
-	constructor (userOptions = {}) {
-    const defaultOptions = {
-      $el: $('.slideshow'),
-      showArrows: false,
-      showPagination: true,
-      duration: 10000,
-      autoplay: true
-    }
-    
-    let options = Object.assign({}, defaultOptions, userOptions);
-    
-		this.$el = options.$el;
-		this.maxSlide = this.$el.find($('.js-slider-home-slide')).length;
-    this.showArrows = this.maxSlide > 1 ? options.showArrows : false;
-    this.showPagination = options.showPagination;
-		this.currentSlide = 1;
-		this.isAnimating = false;
-		this.animationDuration = 1200;
-		this.autoplaySpeed = options.duration;
-		this.interval;
-		this.$controls = this.$el.find('.js-slider-home-button');
-    this.autoplay = this.maxSlide > 1 ? options.autoplay : false;
-
-		this.$el.on('click', '.js-slider-home-next', (event) => this.nextSlide());
-		this.$el.on('click', '.js-slider-home-prev', (event) => this.prevSlide());
-    this.$el.on('click', '.js-pagination-item', event => {
-      if (!this.isAnimating) {
-        this.preventClick();
-  this.goToSlide(event.target.dataset.slide);
-      }
-    });
-
-		this.init();
-	}
-  
-  init() {
-    this.goToSlide(1);
-    if (this.autoplay) {
-      this.startAutoplay();
-    }
-    
-    if (this.showPagination) {
-      let paginationNumber = this.maxSlide;
-      let pagination = '<div class="pagination"><div class="container">';
-      
-      for (let i = 0; i < this.maxSlide; i++) {
-        let item = `<span class="pagination__item js-pagination-item ${ i === 0 ? 'is-current' : ''}" data-slide=${i + 1}>${i + 1}</span>`;
-        pagination  = pagination + item;
-      }
-      
-      pagination = pagination + '</div></div>';
-      
-      this.$el.append(pagination);
-    }
-  }
-  
-  preventClick() {
-		this.isAnimating = true;
-		this.$controls.prop('disabled', true);
-		clearInterval(this.interval);
-
-		setTimeout(() => {
-			this.isAnimating = false;
-			this.$controls.prop('disabled', false);
-      if (this.autoplay) {
-			  this.startAutoplay();
-      }
-		}, this.animationDuration);
-	}
-
-	goToSlide(index) {    
-    this.currentSlide = parseInt(index);
-    
-    if (this.currentSlide > this.maxSlide) {
-      this.currentSlide = 1;
-    }
-    
-    if (this.currentSlide === 0) {
-      this.currentSlide = this.maxSlide;
-    }
-    
-    const newCurrent = this.$el.find('.js-slider-home-slide[data-slide="'+ this.currentSlide +'"]');
-    const newPrev = this.currentSlide === 1 ? this.$el.find('.js-slider-home-slide').last() : newCurrent.prev('.js-slider-home-slide');
-    const newNext = this.currentSlide === this.maxSlide ? this.$el.find('.js-slider-home-slide').first() : newCurrent.next('.js-slider-home-slide');
-    
-    this.$el.find('.js-slider-home-slide').removeClass('is-prev is-next is-current');
-    this.$el.find('.js-pagination-item').removeClass('is-current');
-    
-		if (this.maxSlide > 1) {
-      newPrev.addClass('is-prev');
-      newNext.addClass('is-next');
-    }
-    
-    newCurrent.addClass('is-current');
-    this.$el.find('.js-pagination-item[data-slide="'+this.currentSlide+'"]').addClass('is-current');
-  }
-  
-  nextSlide() {
-    this.preventClick();
-    this.goToSlide(this.currentSlide + 1);
-	}
-   
-	prevSlide() {
-    this.preventClick();
-    this.goToSlide(this.currentSlide - 1);
-	}
-
-	startAutoplay() {
-		this.interval = setInterval(() => {
-			if (!this.isAnimating) {
-				this.nextSlide();
-			}
-		}, this.autoplaySpeed);
-	}
-
-	destroy() {
-		this.$el.off();
-	}
+var options = {
+  imgSrc1:"//s3-us-west-2.amazonaws.com/s.cdpn.io/261873/TelephoneBanana.jpg",
+  imgSrc2:"//s3-us-west-2.amazonaws.com/s.cdpn.io/261873/TelephoneBananaInverted.jpg",
+  containerName : "placeholder",
+  columns:16,
+  margin:3
 }
 
-(function() {
-	let loaded = false;
-	let maxLoad = 3000;  
-  
-	function load() {
-		const options = {
-      showPagination: true
-    };
 
-    let slideShow = new Slideshow(options);
-	}
+function VenetianBlinds(defaults)
+{
+  var cols = defaults.columns;
+  var margin = defaults.margin;
+  var img1 = defaults.imgSrc1;
+  var img2 = defaults.imgSrc2;
+  var placeholder = document.getElementsByClassName(defaults.containerName)[0];
+  var directionX, directionY;
   
-	function addLoadClass() {
-		$body.addClass('is-loaded');
+  var column, blind, blindImg;
+  var bgImg, rot;
+  var colL;
+  var colW = (placeholder.offsetWidth / cols) - margin;
+  for (var i=0; i < cols; i++)
+    {
+      colL = ((colW + margin) * i);
+      
+      column = document.createElement('div');
+      column.className = "column";
+      column.style.width = colW + "px";
+      column.style.left = colL + "px";
+      placeholder.appendChild(column); 
+      
+      for (var j=0; j<4; j++)
+        {
+          blind = document.createElement('div');
+          blind.className = "blind";
+          blind.style.width = colW + "px";
+          blindImg = document.createElement('div');
+          blindImg.className = "blindImg";
+          
+          switch (j){
+             case 0:
+                TweenMax.set(blind, {rotationY: "0"});
+                bgImg = img1;
+                break;
+            case 1:
+                TweenMax.set(blind, {rotationY: "90"});
+                bgImg = img2;
+                break;
+             case 2: 
+                  TweenMax.set(blind, {rotationY: "180"});
+                  bgImg = img1;
+                  break;              
+              case 3:
+                  TweenMax.set(blind, {rotationY: "270"});
+                  bgImg = img2;
+                  break;
+          }
+          blindImg.style.width = placeholder.offsetWidth + "px";
+          blindImg.style.backgroundImage = "url("+bgImg+")";
+          blindImg.style.left = -colL + "px";
 
-		setTimeout(function() {
-			$body.addClass('is-animated');
-		}, 600);
-	}
-  
-	$window.on('load', function() {
-		if(!loaded) {
-			loaded = true;
-			load();
-		}
-	});
-  
-	setTimeout(function() {
-		if(!loaded) {
-			loaded = true;
-			load();
-		}
-	}, maxLoad);
+          column.appendChild(blind);
+          blind.appendChild(blindImg);
+          
+          TweenMax.set(blind, { transformOrigin:"50% 50% " + -colW/2, transformStyle: "preserve-3d"});
+        }
+      
+      TweenMax.set(column, {transformStyle:"preserve-3d", transformPerspective:1000, rotationY:0});
+      
+       column.addEventListener("mouseenter", function(event){
+            var elem = event.currentTarget;
+            var rotY = elem._gsTransform.rotationY;
+         
+            if(directionX > 0){
+              TweenMax.to(elem, 1, {rotationY:Math.floor(rotY/90)*90+90, transformOrigin:"50% 50% -" + colW/2, ease:Back.easeOut});
+            }else{
+              TweenMax.to(elem, 1, {rotationY:Math.floor(rotY/90)*90-90, transformOrigin:"50% 50% -" + colW/2, ease:Back.easeOut});
+            }
+      })
+    }
+      document.addEventListener('mousemove', function (event) {
+      directionX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+      directionY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+    });
+}
 
-	addLoadClass();
-})();
+VenetianBlinds(options);
+
